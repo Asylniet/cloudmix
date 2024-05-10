af40ddb3f35c0be82914d0daf22119a7005c0227
Asylniet <zasylniet@mail.ru>
2024-05-10 16:42:06 +0500

    fix(Module not found: Can't resolve '@/components/Chat')

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import { Chat } from "@/components/Chat";
 import React from "react";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <Chat />
-    </main>
-  );
+  return <main></main>;
 }
